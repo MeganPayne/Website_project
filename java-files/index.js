3866a718c1b3f20e4da102d5a0644aab0cddad44
MeganPayne <m.l.payne@lancaster.ac.uk>
2018-03-14 19:15:07 +0000

    As above

--- java-files/index.js
+++ java-files/index.js
@@ -1,16 +1,16 @@
 
 
-var slideindex=0;
+var slideindex = 0;
 showslides();
 
 function showslides() {
     var i;
     var slides = document.getElementsByClassName("photob");
-    for (i=0; i < slides.length; i++) {
+    for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
     slideindex++;
-    if (slideindex > slides.length) {slideindex=1}
+    if (slideindex > slides.length) {slideindex = 1}
     slides[slideindex-1].style.display ="block";
     setTimeout(showslides, 2000); /*change every 2 seconds*/
 }
