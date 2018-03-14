82b7d8daaeb0b5886a7c610daaeb1ed539f8a00d
MeganPayne <m.l.payne@lancaster.ac.uk>
2018-03-14 14:53:52 +0000

    Attempted the slide show and added java

---
+++ java-files/index.js
@@ -0,0 +1,16 @@
+
+
+var slideindex=0;
+showslides();
+
+function showslides() {
+    var i;
+    var slides = document.getElementsByClassName("photob");
+    for (i=0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideindex++;
+    if (slideindex > slides.length) {slideindex=1}
+    slides[slideindex-1].style.display ="block";
+    setTimeout(showslides, 2000); /*change every 2 seconds*/
+}
